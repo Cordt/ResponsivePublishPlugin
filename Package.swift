28796234989f3f8e22eac0ecd3f5d294f26fc929
Cordt <1705816+Cordt@users.noreply.github.com>
2023-07-05 17:02:37 +0200

    Bumped platform version

--- Package.swift
+++ Package.swift
@@ -1,16 +1,17 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.5
 
 import PackageDescription
 
 let package = Package(
     name: "ResponsivePublishPlugin",
+    platforms: [.macOS(.v12)],
     products: [
         .library(
             name: "ResponsivePublishPlugin",
             targets: ["ResponsivePublishPlugin"]),
     ],
     dependencies: [
-        .package(name: "Publish", url: "https://github.com/johnsundell/publish.git", from: "0.7.0"),
+        .package(name: "Publish", url: "https://github.com/johnsundell/publish.git", from: "0.9.0"),
         .package(name: "SwiftGD", url: "https://github.com/twostraws/SwiftGD.git", from: "2.0.0"),
     ],
     targets: [
